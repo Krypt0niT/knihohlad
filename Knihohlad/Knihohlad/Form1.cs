a2b72bf6fe7dc255f476526d2259ede0fa85e56b
Krypt0niT <k269692@gmail.com>
2023-03-18 11:01:16 +0100

    to ntb

--- Knihohlad/Knihohlad/Form1.cs
+++ Knihohlad/Knihohlad/Form1.cs
@@ -22,59 +22,41 @@ namespace Knihohlad
         private void Form1_Load(object sender, EventArgs e)
         {
             db_connect();
+
         }
         void db_connect()
         {
-            // Change the username, password and database according to your needs
-            // You can ignore the database option if you want to access all of them.
-            // 127.0.0.1 stands for localhost and the default port to connect.
+
             string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=knizky;";
-            // Your query,
             string query = "SELECT * FROM knihy";
 
-            // Prepare the connection
             MySqlConnection databaseConnection = new MySqlConnection(connectionString);
             MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
             commandDatabase.CommandTimeout = 60;
             MySqlDataReader reader;
 
-            // Let's do it !
             try
             {
-                // Open the database
                 databaseConnection.Open();
-
-                // Execute the query
                 reader = commandDatabase.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+                dataGridView1.DataSource = table;
 
-                // All succesfully executed, now do something
-
-                // IMPORTANT : 
-                // If your query returns result, use the following processor :
-
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        // As our database, the array will contain : ID 0, FIRST_NAME 1,LAST_NAME 2, ADDRESS 3
-                        // Do something with every received database ROW
-                        string[] row = { reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetString(3) };
-                        listView1.Items.Add(row[1]);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("No rows found.");
-                }
-
-                // Finally close the connection
-                databaseConnection.Close();
+                
             }
             catch (Exception ex)
             {
-                // Show any error message.
                 MessageBox.Show(ex.Message);
             }
         }
+
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
     }
 }
